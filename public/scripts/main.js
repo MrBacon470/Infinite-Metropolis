09b9594b4038d73684eff6bf9f4b4ae3ede431a0
VoidCloud <72811949+MrBacon470@users.noreply.github.com>
2023-12-19 12:31:39 -0600

    FInal Changes gonna be doing a different project for awhile

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -15,6 +15,23 @@ function initGame() {
     //Town Hall Tab
     //Finances Tab
     //Specialization Tab
+    //Advising Tab
+    const acc = document.getElementsByClassName("accordion");
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+        });
+    } 
     //Settings Tab
     DOMCacheGetOrSet('saveButton').addEventListener('click', () => save())
     DOMCacheGetOrSet('exportButton').addEventListener('click',() => exportSave())
